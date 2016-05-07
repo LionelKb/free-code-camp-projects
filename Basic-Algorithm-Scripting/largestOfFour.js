2c5fa8195ce0b9cb78ada9e82cce4da91237b38e
LionelKb <lionel.kubwimana@gmail.com>
2016-05-07 16:46:37 +0200

    Algo largest elements in sub-arrays

---
+++ Basic-Algorithm-Scripting/largestOfFour.js
@@ -0,0 +1,23 @@
+/*
+
+Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+
+*/
+function largestOfFour(arr) {
+  // You can do this!
+  var largestOfFour=[];
+  for (var i=0;i < arr.length;i++) {
+    var largestOfOne = arr[i].sort(function(a,b){
+      return b - a;
+    })[0];
+    largestOfFour.push(largestOfOne);
+  }
+  return largestOfFour;
+}
+
+
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+console.log(largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+console.log(largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]));
