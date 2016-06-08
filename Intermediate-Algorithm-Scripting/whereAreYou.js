40ab2a53272d681c00e5ab903e69ed7f997562f5
LionelKb <lionel.kubwimana@gmail.com>
2016-06-08 13:44:30 +0200

    1st version

---
+++ Intermediate-Algorithm-Scripting/whereAreYou.js
@@ -0,0 +1,39 @@
+/*
+
+Where art thou
+
+Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching property and value pairs (second argument). Each property and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the property and its value, that was passed on as the second argument.
+
+*/
+
+function whereAreYou(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+
+  var keysOfSource = Object.keys(source);
+  for (var i = 0; i < collection.length; i++) {
+    var itemOfCollection = collection[i];
+    var keysOfItemOfCollection = Object.keys(itemOfCollection);
+    var itemIsPresent = true;
+    for (var j = 0; j < keysOfSource.length; j++) {
+      var prop = keysOfSource[j];
+      itemIsPresent = itemIsPresent && itemOfCollection.hasOwnProperty(prop) && itemOfCollection[prop] == source[prop];
+      }
+      if (itemIsPresent) {
+        arr.push(itemOfCollection);
+      }
+    }
+
+  console.log(arr);
+  // Only change code above this line
+  return arr;
+}
+
+whereAreYou([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+whereAreYou([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }], { "a": 1 }); // should return [{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }].
+
+whereAreYou([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 }); //should return [{ "a": 1, "b": 2 }, { "a": 1, "b": 2, "c": 2 }
