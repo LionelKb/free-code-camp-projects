88c21fb3248c6ebed441962714624fe14b09a179
LionelKb <lionel.kubwimana@gmail.com>
2016-06-17 10:47:09 +0200

    adding tests examples

--- Intermediate-Algorithm-Scripting/convertHTML.js
+++ Intermediate-Algorithm-Scripting/convertHTML.js
@@ -25,5 +25,10 @@ function convertHTML(str) {
   return str;
 }
 
-convertHTML("Dolce & Gabbana");
-convertHTML("Hamburgers < Pizza < Tacos")
+convertHTML("Dolce & Gabbana"); // should return Dolce &​amp; Gabbana.
+convertHTML("Hamburgers < Pizza < Tacos"); // should return Hamburgers &​lt; Pizza &​lt; Tacos.
+convertHTML("Sixty > twelve"); // should return Sixty &​gt; twelve.
+convertHTML('Stuff in "quotation marks"'); // should return Stuff in &​quot;quotation marks&​quot;.
+convertHTML("Shindler's List"); // should return Shindler&​apos;s List.
+convertHTML("<>"); // should return &​lt;&​gt;.
+convertHTML("abc"); // should return abc.
