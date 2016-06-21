85d36fd3ce7af274ca1f0f5283e06dad3e36c86c
LionelKb <lionel.kubwimana@gmail.com>
2016-06-21 16:17:29 +0200

    adding Twitter feature

--- Intermediate-Front-End-Development-Projects/Random-Quote-Machine/script.js
+++ Intermediate-Front-End-Development-Projects/Random-Quote-Machine/script.js
@@ -1,18 +1,29 @@
+var currentQuote="";
+var currentAuthor="";
+
+function getNewQuote() {
+  var apiKey = "457653";
+  var apiMethod = "getQuote";
+  var url = "http://api.forismatic.com/api/1.0/?method="+apiMethod+"&key="+apiKey+"&format=jsonp&lang=en&jsonp=?";
+  $.getJSON(url, function(data) {
+
+    var json = JSON.stringify(data);
+    var quoteData = JSON.parse(json);
+    currentQuote = quoteData.quoteText;
+    currentAuthor = quoteData.quoteAuthor;
+    $("#quote").html(currentQuote);
+    $("#author").html(currentAuthor);
+  });
+}
+
 $(document).ready(function() {
+  getNewQuote();
+  // Generating new quote
+  $("#new-quote").on("click", getNewQuote());
 
-  $("#new-quote").on("click", function(){
-
-      var apiKey = "457653";
-      var apiMethod = "getQuote";
-      var url = "http://api.forismatic.com/api/1.0/?method="+apiMethod+"&key="+apiKey+"&format=jsonp&lang=en&jsonp=?";
-      $.getJSON(url, function(data) {
-
-        var json = JSON.stringify(data);
-        var quoteData = JSON.parse(json);
-        var quote = quoteData.quoteText;
-        var author = quoteData.quoteAuthor;
-        $("#quote").html(quote);
-        $("#author").html(author);
-      });
+  // Tweet the current quote
+  $("#tweet-quote").on("click", function(){
+    var url = 'https://twitter.com/intent/tweet?hashtags=quotes&text="'+currentQuote+'" ' +currentAuthor;
+    $("#tweet-quote").attr("href",url);
   });
 });
