4742cf049acf88ac307ae1522f49478c3b69e405
LionelKb <lionel.kubwimana@gmail.com>
2016-06-23 12:52:30 +0200

    Done! 1st version

---
+++ Intermediate-Algorithm-Scripting/findElement.js
@@ -0,0 +1,17 @@
+/*
+
+Finders Keepers
+
+Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument).
+
+*/
+
+function findElement(arr,func) {
+  var num = arr.filter(func)[0];
+  console.log(num);
+  return num;
+}
+
+findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; }); //should return 2.
+findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; }); //should return 8.
+findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; }); //should return undefined.
