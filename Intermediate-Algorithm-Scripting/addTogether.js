5b1955ca96359607888765d2ca86fdd8d952ec04
LionelKb <lionel.kubwimana@gmail.com>
2016-07-05 11:04:24 +0200

    Done! 1st version

---
+++ Intermediate-Algorithm-Scripting/addTogether.js
@@ -0,0 +1,53 @@
+/*
+
+args Optional
+
+
+Create a function that sums two args together. If only one argument is provided, then return a function that expects one argument and returns the sum.
+
+For example, addTogether(2, 3) ; //should return 5, and addTogether(2) ; //should return a function.
+
+Calling this returned function with a single argument will then return the sum:
+
+var sumTwoAnd = addTogether(2);
+
+sumTwoAnd(3) returns 5.
+
+If either argument isn't a valid number, return undefined.
+
+*/
+
+function addTogether() {
+  // Retrieve the args
+  var args = Array.from(arguments);
+
+  if (!Number.isInteger(args[0])) {
+    return undefined;
+  }
+  // In case of 2 args provided
+  if (args.length === 2) {
+    if (Number.isInteger(args[1])) { //return the addition
+      return args[0] + args[1];
+    }
+    else { // The 2nd argument is NaN, return undefined
+      return undefined;
+    }
+  }
+
+  // Case of one argument
+  return function(value) {
+    if (Number.isInteger(value)) {
+      return value + args[0];
+    }
+
+  };
+}
+
+addTogether(2, 3) ; //should return 5.
+addTogether(2)(3) ; //should return 5.
+addTogether("http://bit.ly/IqT6zt") ; //should return undefined.
+addTogether(2, "3") ; //should return undefined.
+addTogether(2)([3]) ; //should return undefined.
+var sumTwoAnd = addTogether(2);
+console.log(sumTwoAnd(3)) ;//returns 5.
+
